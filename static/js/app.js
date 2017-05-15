2284117cfc893d4d22a727c49abf11f178829cc7
Skylar Ittner <admin@netsyms.com>
2017-05-15 11:48:24 -0600

    add JS to remove &msg=xyz from URL, update material-color to v1.2

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,19 @@ $(document).ready(function () {
         $(this).parent().fadeOut("slow");
     });
 });
+
+
+/*
+ * Remove feedback params from the URL so they don't stick around too long
+ */
+function getniceurl() {
+    var url = window.location.search;
+    url = url.substring(url.lastIndexOf("/") + 1);
+    url = url.replace(/&?msg=([^&]$|[^&]*)/i, "");
+    return url;
+}
+try {
+    window.history.replaceState("", "", getniceurl());
+} catch (ex) {
+    
+}
