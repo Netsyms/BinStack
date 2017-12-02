37789df696189999df5eb25b7a6e39ab661f70c4
Skylar Ittner <admin@netsyms.com>
2017-12-01 22:06:04 -0700

    Trigger warning with missing lang strings

--- required.php
+++ required.php
@@ -136,6 +136,7 @@ function lang($key, $echo = true) {
     if (array_key_exists($key, STRINGS)) {
         $str = STRINGS[$key];
     } else {
+        trigger_error("Language key \"$key\" does not exist in " . LANGUAGE, E_USER_WARNING);
         $str = $key;
     }
 
@@ -158,6 +159,7 @@ function lang2($key, $replace, $echo = true) {
     if (array_key_exists($key, STRINGS)) {
         $str = STRINGS[$key];
     } else {
+        trigger_error("Language key \"$key\" does not exist in " . LANGUAGE, E_USER_WARNING);
         $str = $key;
     }
 
