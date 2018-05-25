dafc3b76ea4e531adc62977184a6cb1708e46f1c
Skylar Ittner <admin@netsyms.com>
2018-05-24 19:52:21 -0600

    Add message if user is kicked out of application for lack of permissions

--- lang/en_us.php
+++ lang/en_us.php
@@ -28,6 +28,7 @@ define("STRINGS", [
     "login server error" => "The login server returned an error: {arg}",
     "login server user data error" => "The login server refused to provide account information.  Try again or contact technical support.",
     "captcha error" => "There was a problem with the CAPTCHA (robot test).  Try again.",
+    "no permission" => "You don't have permission to do that.",
     "home" => "Home",
     "more" => "More",
     "test" => "Test"
