f1a85f47fd6b7e8caef810d7f77db377ba50b29c
Skylar Ittner <admin@netsyms.com>
2018-12-20 23:25:34 -0700

    Add comment

--- api.php
+++ api.php
@@ -4,4 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+
+// Load in new API from legacy location (a.k.a. here)
 require __DIR__ . "/api/index.php";
