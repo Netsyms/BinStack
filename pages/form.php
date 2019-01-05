7173a50c36f7ecec20e368216fa778e1a63265d0
Skylar Ittner <admin@netsyms.com>
2019-01-04 17:53:01 -0700

    Add textarea to FormBuilder

--- pages/form.php
+++ pages/form.php
@@ -18,6 +18,7 @@ $form->addHiddenInput("page", "form");
 
 $form->addInput("name", "John", "text", true, null, null, "Your name", "fas fa-user", 6, 5, 20, "John(ny)?|Steve", "Invalid name, please enter John, Johnny, or Steve.");
 $form->addInput("location", "", "select", true, null, ["1" => "Here", "2" => "There"], "Location", "fas fa-map-marker");
+$form->addInput("textbox", "Hello world", "textarea", true, null, null, "Text area", "fas fa-font");
 $form->addInput("box", "1", "checkbox", true, null, null, "I agree to the terms of service");
 
 $form->addButton("Submit", "fas fa-save", null, "submit", "savebtn");
